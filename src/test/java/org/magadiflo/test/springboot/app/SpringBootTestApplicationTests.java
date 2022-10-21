fb89044a82625c9ecc14d72d8b7ebc01787b509a
Martín <magadiflo@gmail.com>
2022-10-21 12:23:08 -0500

    Test verify

--- src/test/java/org/magadiflo/test/springboot/app/SpringBootTestApplicationTests.java
+++ src/test/java/org/magadiflo/test/springboot/app/SpringBootTestApplicationTests.java
@@ -1,9 +1,12 @@
 package org.magadiflo.test.springboot.app;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.magadiflo.test.springboot.app.data.Datos;
+import org.magadiflo.test.springboot.app.models.Banco;
+import org.magadiflo.test.springboot.app.models.Cuenta;
 import org.magadiflo.test.springboot.app.repositories.IBancoRepository;
 import org.magadiflo.test.springboot.app.repositories.ICuentaRepository;
 import org.magadiflo.test.springboot.app.services.ICuentaService;
@@ -16,36 +19,49 @@ import java.math.BigDecimal;
 @SpringBootTest
 class SpringBootTestApplicationTests {
 
-	ICuentaRepository cuentaRepository;
-	IBancoRepository bancoRepository;
-	ICuentaService cuentaService;
+    ICuentaRepository cuentaRepository;
+    IBancoRepository bancoRepository;
+    ICuentaService cuentaService;
 
-	@BeforeEach
-	void setUp() {
-		this.cuentaRepository = Mockito.mock(ICuentaRepository.class);
-		this.bancoRepository = Mockito.mock(IBancoRepository.class);
-		this.cuentaService = new CuentaServiceImpl(cuentaRepository, bancoRepository);
-	}
+    @BeforeEach
+    void setUp() {
+        this.cuentaRepository = Mockito.mock(ICuentaRepository.class);
+        this.bancoRepository = Mockito.mock(IBancoRepository.class);
+        this.cuentaService = new CuentaServiceImpl(cuentaRepository, bancoRepository);
+    }
 
-	@Test
-	void contextLoads() {
-		Mockito.when(this.cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
-		Mockito.when(this.cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
-		Mockito.when(this.bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+    @Test
+    void contextLoads() {
+        Mockito.when(this.cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
+        Mockito.when(this.cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
+        Mockito.when(this.bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
 
-		BigDecimal saldoOrigen = this.cuentaService.revisarSaldo(1L);
-		BigDecimal saldoDestino = this.cuentaService.revisarSaldo(2L);
+        BigDecimal saldoOrigen = this.cuentaService.revisarSaldo(1L);
+        BigDecimal saldoDestino = this.cuentaService.revisarSaldo(2L);
 
-		assertEquals("1000", saldoOrigen.toPlainString());
-		assertEquals("2000", saldoDestino.toPlainString());
+        assertEquals("1000", saldoOrigen.toPlainString());
+        assertEquals("2000", saldoDestino.toPlainString());
 
-		this.cuentaService.transferir(1L, 2L, new BigDecimal("100"), 1L);
+        this.cuentaService.transferir(1L, 2L, new BigDecimal("100"), 1L);
 
-		saldoOrigen = this.cuentaService.revisarSaldo(1L);
-		saldoDestino = this.cuentaService.revisarSaldo(2L);
+        saldoOrigen = this.cuentaService.revisarSaldo(1L);
+        saldoDestino = this.cuentaService.revisarSaldo(2L);
 
-		assertEquals("900", saldoOrigen.toPlainString());
-		assertEquals("2100", saldoDestino.toPlainString());
-	}
+        assertEquals("900", saldoOrigen.toPlainString());
+        assertEquals("2100", saldoDestino.toPlainString());
+
+        int total = this.cuentaService.revisarTotalTransferencias(1L);
+
+        assertEquals(1, total);
+
+        //Verificamos el número de veces que se llaman los métodos de cada repository
+        Mockito.verify(this.cuentaRepository, Mockito.times(3)).findById(1L);
+        Mockito.verify(this.cuentaRepository, Mockito.times(3)).findById(2L);
+        Mockito.verify(this.cuentaRepository, Mockito.times(2))
+                .update(Mockito.any(Cuenta.class));
+
+        Mockito.verify(this.bancoRepository, Mockito.times(2)).findById(1L);
+        Mockito.verify(this.bancoRepository).update(Mockito.any(Banco.class));
+    }
 
 }
