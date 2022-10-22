58c66094eea3404d18d8e72effb57725bc0de5ab
Martín <magadiflo@gmail.com>
2022-10-21 19:24:25 -0500

    Modificando nuestros repositorios con Spring Data JPA parte 2

--- src/main/java/org/magadiflo/test/springboot/app/repositories/ICuentaRepository.java
+++ src/main/java/org/magadiflo/test/springboot/app/repositories/ICuentaRepository.java
@@ -2,7 +2,15 @@ package org.magadiflo.test.springboot.app.repositories;
 
 import org.magadiflo.test.springboot.app.models.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ICuentaRepository extends JpaRepository<Cuenta, Long> {
+
+    Optional<Cuenta> findByPersona(String persona);
+
+    @Query("SELECT c FROM Cuenta AS c WHERE c.persona = ?1")
+    Optional<Cuenta> encuentraPorPersona(String persona);
 
 }
