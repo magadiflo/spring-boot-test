32caff16aeb1de79e5b138fce7a5f61311525fa7
Martín <magadiflo@gmail.com>
2022-10-21 19:50:50 -0500

    Escribiendo pruebas para el save

--- src/test/java/org/magadiflo/test/springboot/app/IntegracionJpaTest.java
+++ src/test/java/org/magadiflo/test/springboot/app/IntegracionJpaTest.java
@@ -8,6 +8,7 @@ import org.magadiflo.test.springboot.app.repositories.ICuentaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -51,5 +52,21 @@ public class IntegracionJpaTest {
         List<Cuenta> cuentas = this.cuentaRepository.findAll();
         assertFalse(cuentas.isEmpty());
         assertEquals(2, cuentas.size());
+    }
+
+    @Test
+    void testSave() {
+        // Given
+        Cuenta cuentaPepe = new Cuenta(null, "Pepe", new BigDecimal("3000"));
+
+        // When
+        Cuenta cuenta = this.cuentaRepository.save(cuentaPepe);
+        //Cuenta cuenta = this.cuentaRepository.findByPersona("Pepe").orElseThrow();
+        //Cuenta cuenta = this.cuentaRepository.findById(cuentaSave.getId()).orElseThrow();
+
+        // Then
+        assertEquals("Pepe", cuenta.getPersona());
+        assertEquals("3000", cuenta.getSaldo().toPlainString());
+        //assertEquals(3, cuenta.getId()); no es recomendable usar el id ya que podría cambiar
     }
 }
