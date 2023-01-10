77ba288dc4ec0305215ec46f0b779909076784f1
Martín <magadiflo@gmail.com>
2023-01-10 11:35:47 -0500

    Agreando información - Spring Boot: Test de Integración de servicios Rest con WebTestClient

--- src/test/java/org/magadiflo/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
+++ src/test/java/org/magadiflo/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
@@ -50,6 +50,89 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Prueba de Integración
+ * ********************
+ *
+ * Probaremos el controlador CuentaController, pero usando pruebas de integración real,
+ * sin MockMvc, no simular el request, el response, ni el servidor http, etc. sino que
+ * sean peticiones http reales, consumir un api rest de nuestro backend, o sea del
+ * controlador y realizar pruebas, pero todo real.
+ *
+ * Levantaremos un servidor real que estará desplegada en algún puerto con el objeto
+ * HttpServletRequest y Response pero reales, no Mocks.
+ *
+ * A todo lo anterior se le conoce como Pruebas de integración a servicios rest, por lo
+ * tanto cada vez que realicemos nuestras pruebas de integración, la aplicación que estemos
+ * probando debe estar levantada, es decir en este caso la application que contenga el
+ * CuentaController debe estar levantado para que se pueda comunicar, de lo contrario
+ * no se va a poder establecer la comunicación a los endpoints.
+ *
+ * Es una prueba distinta, en este caso usando clientes http como RestTemplate o WebClient,
+ * dos tecnologías que usa spring para consumir servicios rest.
+ * */
+
+/**
+ * SpringBootTest.WebEnvironment.RANDOM_PORT
+ * *****************************************
+ * Para que automáticamente nuestras pruebas unitarias levanten un servidor real
+ * en un puerto aleatorio. Este servidor tendrá el contexto de aplicación de SpringBoot,
+ * quien nos permitirá hacer inyección de dependencia usando el @Autowired.
+ *
+ * Cuando estamos en el mismo proyecto, nuestras pruebas unitarias (clase test) o sea
+ * en el mismo proyecto del backend (controlador que queremos probar) no es necesario
+ * colocar toda la url, ejm:
+ *      this.client.post().uri("http://localhost:8080/api/cuentas/transferir")...
+ * simplemente podemos colocar:
+ *      this.client.post().uri("/api/cuentas/transferir")...
+ * ¿Cuál es la diferencia?, es que con la segunda forma solo levantamos una instancia, un
+ * servidor, el de las pruebas, lo que significa que tanto aplicación del backend, como las
+ * pruebas a ejecutar estarán en el mismo servidor. Mientras que con el de la primera forma
+ * (colocando toda la url) necesitamos levantar primero el proyecto donde está el controlador
+ * a probar, que por defecto se levanta en el puerto 8080 y luego habría que ejecutar las
+ * pruebas y este se levante con un puerto aleatorio.
+ */
+
+/**
+ * WebTestClient
+ * *************
+ * WebClient, es la implementación real para consumir servicios Rest, pero en este caso
+ * necesitamos el WebTestClient que tiene características de pruebas unitarias.
+ *
+ * Con WebTestClient, enviamos el DTO en el cuerpo del request, pero este DTO
+ * en automático será convertido por WebTestClient en un objeto JSON, es decir, no es
+ * necesario convertirlo con el ObjectMapper, tal como lo hacíamos en MockMvc.
+ *
+ * Ahora, si en la respuesta del WebTestClient nos trae un objeto json y queremos
+ * probar el objeto completo, es necesario convertir primero el objeto esperado
+ * con el ObjectMapper a un objeto JSON.
+ */
+
+/**
+ * @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+ * *****************************************************
+ * Con esta anotación a nivel de clase ordenaremos los métodos test
+ * mediante la anotación @Order(...)
+ *
+ * ¡Ojo! Como es una prueba contra una base de datos real,
+ * dependiendo de qué tests se ejecuten primero, estos pueden
+ * modificar los datos de la BD y eso puede afectar la
+ * ejecución de los test siguientes.
+ *
+ * Como recomendación, cuando utilizamos estas pruebas de integración
+ * y tenemos métodos test que algunos modifican datos podríamos dar
+ * algún tipo de prioridad, es decir, primero que se ejecute este
+ * método, luego este otro, etc... un orden, pero solo en estas
+ * PRUEBAS DE INTEGRACIÓN, para que un método que se ejecutó antes
+ * no afecte a otro que se ejecute después
+ */
+
+/**
+ * Dos Formas de hacer comprobaciones de las respuestas obtenidas
+ * **************************************************************
+ * FORMA 1° usando la forma nativa del .jsonPath(...)
+ * FORMA 2° usando el consumeWith(...)
+ */
 @Tag(value = "integracion_web_client")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -86,12 +169,14 @@ class CuentaControllerWebTestClientTest {
         this.client.post().uri("/api/cuentas/transferir")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(dto) //Por debajo ser convierte en JSON
-                .exchange() //Para enviar la solicitud
+                .exchange() //Para enviar la solicitud. Lo que venga después del .exchange() es la respuesta.
 
                 // THEN
                 .expectStatus().isOk()
                 .expectBody()
-                .consumeWith(respuesta -> {
+
+                // FORMA 2° usando el consumeWith(...)
+                .consumeWith(respuesta -> { //Expresión lambda que permitirá consumir la respuesta y hacer los asserts con él
                     // Queremos convertir la respuesta en una estructura json
                     try {
                         JsonNode json = this.objectMapper.readTree(respuesta.getResponseBody());
@@ -104,21 +189,23 @@ class CuentaControllerWebTestClientTest {
                         throw new RuntimeException(e);
                     }
                 })
+
+                // FORMA 1° usando la forma nativa del .jsonPath
                 .jsonPath("$.mensaje").isNotEmpty()
                 .jsonPath("$.mensaje").value(Matchers.is("Transferencia realizada con éxito"))
                 .jsonPath("$.mensaje").value(valor -> assertEquals("Transferencia realizada con éxito", valor))
                 .jsonPath("$.mensaje").isEqualTo("Transferencia realizada con éxito")
                 .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(dto.getCuentaOrigenId())
                 .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
+
+                // Si queremos probar el json completo, es necesario tener el ObjectMapper para hacer la conversión
                 .json(this.objectMapper.writeValueAsString(response));
     }
 
-    // A continuación se muestran dos formas de hacer la comprobación:
-    // 1) Con jsonPath(...) ----> testDetalle()
-    // 2) con consumeWith(...) ---> testDetalle2()
     @Test
     @Order(1)
-    void testDetalle() throws Exception {
+    @DisplayName(value = "probando el método handler detalle() y haciendo la comprobación con jsonPath()")
+    void testDetalleConJsonPath() throws Exception {
         Cuenta cuenta = new Cuenta(1L, "Martín", new BigDecimal("1000"));
 
         this.client.get().uri("/api/cuentas/1")
@@ -132,10 +219,19 @@ class CuentaControllerWebTestClientTest {
 
     @Test
     @Order(2)
-    void testDetalle2() {
+    @DisplayName(value = "probando el método handler detalle() y haciendo la comprobación con consumeWith()")
+    void testDetalleConConsumeWith() {
         this.client.get().uri("/api/cuentas/2")
                 .exchange() //Realizamos el request
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                /**
+                 * expectBody(Cuenta.class)
+                 * ************************
+                 * Colocamos dentro la clase Cuenta, porque los atributos devueltos
+                 * en el JSON son iguales a los atributos de la clase Cuenta. Y eso es,
+                 * porque el método handler de esa uri retorna un objeto del tipo
+                 * de la clase Cuenta.
+                 */
                 .expectBody(Cuenta.class)
                 .consumeWith(resp -> {
                     Cuenta cuenta = resp.getResponseBody();
@@ -148,7 +244,8 @@ class CuentaControllerWebTestClientTest {
 
     @Test
     @Order(4)
-    void testListar() {
+    @DisplayName(value = "probando el método listar con JsonPath que viene en una estructura de arreglo")
+    void testListarConJsonPath() {
         this.client.get().uri("/api/cuentas").exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -165,7 +262,8 @@ class CuentaControllerWebTestClientTest {
 
     @Test
     @Order(5)
-    void testListar2() {
+    @DisplayName(value = "probando el método listar con expectBodyList y consumeWith")
+    void testListarConConsumeWith() {
         this.client.get().uri("/api/cuentas").exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -175,6 +273,7 @@ class CuentaControllerWebTestClientTest {
 
                     assertNotNull(cuentas);
                     assertEquals(2, cuentas.size());
+
                     assertEquals(1L, cuentas.get(0).getId());
                     assertEquals("Martín", cuentas.get(0).getPersona());
                     assertEquals(Double.parseDouble("900"), cuentas.get(0).getSaldo().doubleValue());
@@ -189,7 +288,8 @@ class CuentaControllerWebTestClientTest {
 
     @Test
     @Order(6)
-    void testGuardar() {
+    @DisplayName(value = "probando el método guardar, usando el JsonPath para la comprobación")
+    void testGuardarConJsonPath() {
         // GIVEN
         Cuenta cuenta = new Cuenta(null, "Tinkler", new BigDecimal("3000"));
 
@@ -210,7 +310,8 @@ class CuentaControllerWebTestClientTest {
 
     @Test
     @Order(7)
-    void testGuardar2() {
+    @DisplayName(value = "probando el método guardar, usando el consumeWith")
+    void testGuardarConConsumeWith() {
         // GIVEN
         Cuenta cuenta = new Cuenta(null, "Pepe", new BigDecimal("4000"));
 
@@ -256,7 +357,22 @@ class CuentaControllerWebTestClientTest {
                 .hasSize(3);
 
         this.client.get().uri("/api/cuentas/4").exchange()
-//                .expectStatus().is5xxServerError();
+                /**
+                 * Error genérico
+                 * ***************
+                 * Al hacer la búsqueda de una cuenta que no existe, el método lanza un
+                 * NoSuchElementException, que al final el servidor lo traduce en un error 500.
+                 */
+                //.expectStatus().is5xxServerError();
+
+
+                /**
+                 * Error personalizado
+                 * *******************
+                 * El error notFound() es lanzado cuando en el método detalle no se encuentra la cuenta,
+                 * pero para eso tuvimos que utilizar el try-catch, capturar la excepción NoSuchElementException,
+                 * y retornar el NotFound()
+                 */
                 .expectStatus().isNotFound()
                 .expectBody().isEmpty();
     }
