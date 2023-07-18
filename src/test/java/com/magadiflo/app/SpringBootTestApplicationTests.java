a8fe288d775bef1a3d4787e208bfcdd66d6bd950
Martín <magadiflo@gmail.com>
2023-07-17 20:09:31 -0500

    Inicio

---
+++ src/test/java/com/magadiflo/app/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
